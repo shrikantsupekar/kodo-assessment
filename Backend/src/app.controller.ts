34fab2954a0b195a2150d1a35910e71c8e2e7492
shrikant <shrikantsupekar96@gmail.com>
2022-06-16 01:58:24 +0530

    implemented API endpoint

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { postDto } from "./app.dto";
 @Controller()
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getPosts(): Array<postDto> {
-    return this.appService.getPosts();
+  getPosts(
+    @Query("query") query: string,
+    @Query("skip") skip: number,
+    @Query("limit") limit: number
+  ): { posts: Array<postDto>; total: number } {
+    return this.appService.getPosts(query, +skip, +limit);
   }
 }
--- Backend/src/app.service.ts
+++ Backend/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { postDto } from "./app.dto";
+import { postsModel } from "./posts.model";
 @Injectable()
 export class AppService {
-  getPosts(): Array<postDto> {
-    return [];
+  getPosts(
+    query: string,
+    skip: number,
+    limit: number
+  ): { posts: Array<postDto>; total: number } {
+    if (query) {
+      return new postsModel().search(query).skip(skip).limit(limit).get();
+    }
+    return new postsModel().skip(skip).limit(limit).get();
   }
 }
